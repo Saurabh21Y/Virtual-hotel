8017e6bdea22c6600fdd03ce9eba35efa08b9e59
Saurabh21Y <157530623+Saurabh21Y@users.noreply.github.com>
2024-02-16 16:47:16 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,97 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOTEL HAVEN-contact</title>
+    <?php require('inc/links.php');?>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"
+    />
+    
+</head>
+<body class="bg-light">
+  
+ <?php require('inc/header.php');?>
+<!-- start -->
+
+<!-- --------------------- -->
+<div class="my-5 px-4">
+  <h2 class="fw-bold h-font text-center">CONTACT US</h2>
+  <hr>
+  <p class="text-center mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+    Maxime quam placeat eos adipisci magnam repudiandae consectetur, dolore temporibus harum consequuntur?
+  </p>
+</div>
+<!-- ---------------------- -->
+<div class="container">
+  <div class="row">
+    <!-- map -->
+    <div class="col-lg-6 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4">
+              <iframe class="w-100 rounded mb-4" height="320" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d239487.16522882093!2d85.65563887753197!3d20.300807014747665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a1909d2d5170aa5%3A0xfc580e2b68b33fa8!2sBhubaneswar%2C%20Odisha!5e0!3m2!1sen!2sin!4v1695330725604!5m2!1sen!2sin"  loading="lazy"></iframe>
+                <h5>Adddress</h5>
+                <a href="https://maps.app.goo.gl/bPYdsxknXfEckqDZ6" target="_blank" class="d-inline-block text-decoration-none text-dark mb-2">
+                  <i class="bi bi-geo-alt-fill"></i>Hotel haven,Bhubaneswar,Odisha
+                </a>
+    <!-- call -->
+                <h5 class="mt-4">Call Us</h5>
+                   <a href="tel: +916202589259" class="d-inline-block mb-2 text-decoration-none text-dark">
+                    <i class="bi bi-telephone-outbound-fill"></i> +916202589259
+                  </a>
+              <br>
+                  <a href="tel: +916202589259" class="d-inline-block mb-2 text-decoration-none text-dark">
+                    <i class="bi bi-telephone-outbound-fill"></i> +916202589259
+                  </a>
+    <!-- email -->
+                  <h5 class="mt-4">Email</h5>
+                  <a href="mailto: abs@gmailcom" class="d-inline-block mb-2 text-decoration-none text-dark">
+                  <i class="bi bi-envelope-fill"></i> abs@gmailcom
+                  </a>
+    <!-- follow us -->
+    <h5 class="mt-4">Follow us</h5>
+              <a href="#" class="d-inline-block mb-3 text-dark fs-5 me-2">
+                  <i class="bi bi-instagram me-1"></i> 
+              </a>
+              <a href="#" class="d-inline-block mb-3 text-dark fs-5 me-2">
+                  <i class="bi bi-facebook me-1"></i> 
+              </a>
+              <a href="#" class="d-inline-block mb-3 text-dark fs-5 me-2">
+                  <i class="bi bi-twitter me-1"></i> 
+              </a>
+            </div>
+    </div>
+    <!-- gym -->
+    <div class="col-lg-6 col-md-6 px-4">
+            <div class="bg-white rounded shadow p-4">
+                <form>
+                  <h5>send a message</h5>
+                  <div class="mt-3">
+                    <label class="form-label" style="font-weight:500;">Name</label>
+                    <input type="text" class="form-control shadow-none">
+                 </div>
+                 <div class="mt-3">
+                    <label class="form-label" style="font-weight:500;">E-mail</label>
+                    <input type="email" class="form-control shadow-none">
+                 </div>
+                 <div class="mt-3">
+                    <label class="form-label" style="font-weight:500;">Subject</label>
+                    <input type="text" class="form-control shadow-none">
+                 </div>
+                 <div class="mt-3">
+                    <label class="form-label" style="font-weight:500;">Message</label>
+                    <textarea class="form-control shadow-none" rows="5" style="resize: none;"></textarea>
+                 </div>
+                 <button type="submit" class="btn btn-dark mt-3">SUBMIT</button>
+                </form>
+            </div>
+      </div>
+      <!-- end of services section  -->
+    </div>
+    </div>
+    
+
+<!-- end -->
+<?php require('inc/footer.php');?>
+
+
+</body>
+</html>
